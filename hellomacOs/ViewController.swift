4615118ce8d7a627f1bb62ab9e3f58f6895f82b2
Santosh A <santu0002@gmail.com>
2017-03-01 03:31:09 +0530

    Initial Commit

---
+++ hellomacOs/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  hellomacOs
+//
+//  Created by Santosh A on 3/1/17.
+//  Copyright © 2017 Santosh A. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
